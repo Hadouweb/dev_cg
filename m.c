5d3a3c33a8e8829931cbb28b147f0a2cca6b0a6f
hadouweb <kengo14@hotmail.fr>
2016-04-01 04:45:37 +0200

    easy with elevator pos

--- m.c
+++ m.c
@@ -2,24 +2,31 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct  s_elev
+{
+    int         y;
+    int         x;
+}               t_elev;
+
 typedef struct  s_app
 {
-    int         map_y; // number of floors
-    int         map_x; // width of the area
-    int         nb_rounds; // maximum number of rounds
-    int         exit_y; // floor on which the exit is found
-    int         exit_x; // position of the exit on its floor
-    int         nb_total_clones; // number of generated clones
-    int         nb_additional_elevators; // ignore (always zero)
-    int         nb_elevators; // number of elevators
-    int         start_x;
-    int         start_y;
+    int         map_y;
+    int         map_x;
+    int         nb_rounds;
+    int         exit_y;
+    int         exit_x;
+    int         nb_total_clones;
+    int         nb_elevators;
+    int         nb_additional_elevators;
+    int         y;
     int         stop_left;
     int         stop_right;
     int         dist;
     int         new_y;
     int         block;
     int         init_x;
+    t_elev      *elev;
+    t_elev      current_elev;
 }               t_app;
 
 typedef struct      s_clone
@@ -29,144 +36,95 @@ typedef struct      s_clone
     char            direction[10];
 }                   t_clone;
 
-void    ft_debug_data(t_app *app)
+void    ft_debug(t_app *app)
 {
-    fprintf(stderr, "map_y : %d\n", app->map_y);
-    fprintf(stderr, "map_x : %d\n", app->map_x);
-    fprintf(stderr, "nombre maximum de tours : %d\n", app->nb_rounds);
-    fprintf(stderr, "exit y : %d\n", app->exit_y);
-    fprintf(stderr, "exit x : %d\n", app->exit_x);
-    fprintf(stderr, "le nombre de clones qui sortiront : %d\n", app->nb_total_clones);
-    //fprintf(stderr, "0 : %d\n", app->nb_additional_elevators);
-    fprintf(stderr, "le nombre d'ascenseurs présents : %d\n", app->nb_elevators);
+    int     i;
+
+    i = 0;
+    while (i < app->nb_elevators)
+    {
+        fprintf(stderr, "elev y x : %d %d\n", app->elev[i].y, app->elev[i].x);
+        i++;
+    }
+    fprintf(stderr, "exit y x : %d %d\n", app->exit_y, app->exit_x);
 }
 
 void    ft_debug_clone(t_clone *clone, t_app *app)
 {
-    fprintf(stderr, "clone y : %d\n", clone->y);
-    fprintf(stderr, "clone x : %d\n", clone->x);
+    fprintf(stderr, "clone y x : %d %d\n", clone->y, clone->x);
     fprintf(stderr, "direction du clone : %s\n", clone->direction);
-    fprintf(stderr, "tours restants : %d\n", app->nb_rounds);
-    fprintf(stderr, "stop_left : %d\n", app->stop_left);
-    fprintf(stderr, "stop_right : %d\n", app->stop_right);
-    fprintf(stderr, "start y x : %d %d\n", app->start_x, app->start_y);
-    fprintf(stderr, "dist max : %d\n", app->dist);
+    fprintf(stderr, "elev y x : %d %d\n", app->current_elev.y, app->current_elev.x);
 }
 
-void    ft_set_stop_right(t_app *app, t_clone *clone)
+void    ft_set_current_elev(t_app *app, int y)
 {
-    app->dist = abs(((app->nb_rounds - app->exit_y) / app->exit_y / 2) - app->exit_x);
-    app->stop_right = app->dist;
-    if (app->stop_right > app->map_x - 1)
-        app->stop_right = app->map_x - 1;
-    app->stop_left = 0;
-    app->new_y = 0;
-}
- 
-void    ft_set_stop_left(t_app *app, t_clone *clone)
-{
-    app->dist = abs(((app->nb_rounds - app->exit_y) / app->exit_y / 2) - app->exit_x);
-    app->stop_left = app->dist;
-    if (app->stop_left < 0)
-        app->stop_left = 0;
-    app->stop_right = app->map_x - 1;
-    app->new_y = 0;
+    int     i;
+
+    i = 0;
+    while (i < app->nb_elevators)
+    {
+        if (app->elev[i].y == y)
+            app->current_elev = app->elev[i];
+        i++;
+    }
 }
 
 void    ft_choice(t_app *app, t_clone *clone)
 {
-    if (clone->y > app->start_y)
+    if (app->current_elev.x == clone->x)
+        ft_set_current_elev(app, clone->y);
+    if (clone->y == app->exit_y)
     {
-        app->start_y = clone->y;
-        app->start_x = clone->x;
-        app->new_y = 1;
+        app->current_elev.x = app->exit_x;
+        app->current_elev.y = app->exit_y;
     }
     if (clone->y == -1)
         printf("WAIT\n");
-    else if (clone->y == app->exit_y)
-    {
-        if (strcmp(clone->direction, "RIGHT") == 0 && clone->x > app->exit_x)
-            printf("BLOCK\n");
-        else if (strcmp(clone->direction, "LEFT") == 0 && clone->x < app->exit_x)
-            printf("BLOCK\n");
-        else
-            printf("WAIT\n");
-    }
+    else if (strcmp(clone->direction, "LEFT") == 0 && app->current_elev.x > clone->x &&
+        app->current_elev.y == clone->y)
+        printf("BLOCK\n");
+    else if (strcmp(clone->direction, "RIGHT") == 0 && app->current_elev.x < clone->x &&
+        app->current_elev.y == clone->y)
+        printf("BLOCK\n");
     else
-    {
-        app->block = 0;
-        if (strcmp(clone->direction, "RIGHT") == 0)
-        {
-            if (app->new_y && clone->y != 0)
-                ft_set_stop_right(app, clone);
-            if (clone->x >= app->stop_right && clone->y == app->start_y)
-            {
-                app->stop_right = app->start_x;
-                printf("BLOCK\n");
-                app->block = 1;
-            }
-        }
-        else if (strcmp(clone->direction, "LEFT") == 0)
-        {
-            if (app->new_y && clone->y != 0)
-                ft_set_stop_left(app, clone);
-            if (clone->x <= app->stop_left && clone->y == app->start_y)
-            {
-                app->stop_left = app->start_x;
-                printf("BLOCK\n");
-                app->block = 1;
-            }
-        }
-        if (!app->block)
-            printf("WAIT\n");
-    }
-    //printf("WAIT\n");
-    app->nb_rounds--;
+        printf("WAIT\n");
 }
 
 void    ft_loop(t_app *app)
 {
     t_clone     clone;
-    int         i;
     int         init;
 
-   // clone = (t_clone*)malloc(app->nb_total_clones * sizeof(t_clone));
-    i = 0;
-    init = 0;
+    init = 1;
     while (1)
     {
         scanf("%d%d%s", &clone.y, &clone.x, &clone.direction);
-        if (init == 0)
+        if (init)
         {
-            app->start_y = clone.y;
-            app->start_x = clone.x;
-            app->init_x = clone.x;
-            app->new_y = 1;
-            init = 1;
+            app->y = clone.y;
+            init = 0;
         }
         ft_choice(app, &clone);
         ft_debug_clone(&clone, app);
-        i++;
     }
 }
 
 int     main()
 {
     t_app   app;
-    int     elevator_floor;
-    int     elevator_pos;
     int     i;
 
     bzero(&app, sizeof(t_app));
-    elevator_floor = 0;
-    elevator_pos = 0;
     scanf("%d%d%d%d%d%d%d%d", &app.map_y, &app.map_x, &app.nb_rounds, &app.exit_y,
         &app.exit_x, &app.nb_total_clones, &app.nb_additional_elevators, &app.nb_elevators);
-    //ft_stop(&app);
-    app.stop_right = app.map_x - 1;
-    i = app.nb_elevators;
-    while (i--)
-        scanf("%d%d", &elevator_floor, &elevator_pos);
-    ft_debug_data(&app);
+    app.elev = (t_elev*)malloc(app.nb_elevators * sizeof(t_elev));
+    i = 0;
+    while (i < app.nb_elevators)
+    {
+        scanf("%d%d", &app.elev[i].y, &app.elev[i].x);
+        i++;
+    }
+    ft_set_current_elev(&app, 0);
+    ft_debug(&app);
     ft_loop(&app);
 }
