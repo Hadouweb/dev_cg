a0085afb2d3ca110e2e3dc51c84c9c0c853a91e7
hadouweb <kengo14@hotmail.fr>
2016-04-01 05:02:11 +0200

    clean code

--- m.c
+++ m.c
@@ -2,129 +2,42 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct  s_elev
+int main()
 {
-    int         y;
-    int         x;
-}               t_elev;
-
-typedef struct  s_app
-{
-    int         map_y;
-    int         map_x;
-    int         nb_rounds;
-    int         exit_y;
-    int         exit_x;
-    int         nb_total_clones;
-    int         nb_elevators;
-    int         nb_additional_elevators;
-    int         y;
-    int         stop_left;
-    int         stop_right;
-    int         dist;
-    int         new_y;
-    int         block;
-    int         init_x;
-    t_elev      *elev;
-    t_elev      current_elev;
-}               t_app;
-
-typedef struct      s_clone
-{
-    int             y;
-    int             x;
-    char            direction[10];
-}                   t_clone;
-
-void    ft_debug(t_app *app)
-{
-    int     i;
-
-    i = 0;
-    while (i < app->nb_elevators)
-    {
-        fprintf(stderr, "elev y x : %d %d\n", app->elev[i].y, app->elev[i].x);
-        i++;
+    int nbFloors; // number of floors
+    int width; // width of the area
+    int nbRounds; // maximum number of rounds
+    int exitFloor; // floor on which the exit is found
+    int exitPos; // position of the exit on its floor
+    int nbTotalClones; // number of generated clones
+    int nbAdditionalElevators; // ignore (always zero)
+    int nbElevators; // number of elevators
+    int *elevator;
+    
+    scanf("%d%d%d%d%d%d%d%d", &nbFloors, &width, &nbRounds, &exitFloor, &exitPos, &nbTotalClones, &nbAdditionalElevators, &nbElevators);
+    elevator = (int*)malloc(nbElevators * sizeof(int));
+    for (int i = 0; i < nbElevators; i++) {
+        int elevatorFloor; // floor on which this elevator is found
+        int elevatorPos; // position of the elevator on its floor
+        scanf("%d%d", &elevatorFloor, &elevatorPos);
+        elevator[elevatorFloor] = elevatorPos;
     }
-    fprintf(stderr, "exit y x : %d %d\n", app->exit_y, app->exit_x);
-}
+    elevator[exitFloor] = exitPos;
 
-void    ft_debug_clone(t_clone *clone, t_app *app)
-{
-    fprintf(stderr, "clone y x : %d %d\n", clone->y, clone->x);
-    fprintf(stderr, "direction du clone : %s\n", clone->direction);
-    fprintf(stderr, "elev y x : %d %d\n", app->current_elev.y, app->current_elev.x);
-}
-
-void    ft_set_current_elev(t_app *app, int y)
-{
-    int     i;
-
-    i = 0;
-    while (i < app->nb_elevators)
-    {
-        if (app->elev[i].y == y)
-            app->current_elev = app->elev[i];
-        i++;
+    // game loop
+    while (1) {
+        int     cloneFloor; // floor of the leading clone
+        int     clonePos; // position of the leading clone on its floor
+        char    direction[2]; // direction of the leading clone: LEFT or RIGHT
+        scanf("%d%d%s", &cloneFloor, &clonePos, direction);
+        
+        if (cloneFloor == -1 || elevator[cloneFloor] == clonePos
+            || ((direction[0] == 'L') && (elevator[cloneFloor] < clonePos))
+            || ((direction[0] == 'R') && (elevator[cloneFloor] > clonePos)))
+            printf("WAIT\n"); // action: WAIT or BLOCK
+        else
+            printf("BLOCK\n"); // action: WAIT or BLOCK   
     }
-}
 
-void    ft_choice(t_app *app, t_clone *clone)
-{
-    if (app->current_elev.x == clone->x)
-        ft_set_current_elev(app, clone->y);
-    if (clone->y == app->exit_y)
-    {
-        app->current_elev.x = app->exit_x;
-        app->current_elev.y = app->exit_y;
-    }
-    if (clone->y == -1)
-        printf("WAIT\n");
-    else if (strcmp(clone->direction, "LEFT") == 0 && app->current_elev.x > clone->x &&
-        app->current_elev.y == clone->y)
-        printf("BLOCK\n");
-    else if (strcmp(clone->direction, "RIGHT") == 0 && app->current_elev.x < clone->x &&
-        app->current_elev.y == clone->y)
-        printf("BLOCK\n");
-    else
-        printf("WAIT\n");
-}
-
-void    ft_loop(t_app *app)
-{
-    t_clone     clone;
-    int         init;
-
-    init = 1;
-    while (1)
-    {
-        scanf("%d%d%s", &clone.y, &clone.x, &clone.direction);
-        if (init)
-        {
-            app->y = clone.y;
-            init = 0;
-        }
-        ft_choice(app, &clone);
-        ft_debug_clone(&clone, app);
-    }
-}
-
-int     main()
-{
-    t_app   app;
-    int     i;
-
-    bzero(&app, sizeof(t_app));
-    scanf("%d%d%d%d%d%d%d%d", &app.map_y, &app.map_x, &app.nb_rounds, &app.exit_y,
-        &app.exit_x, &app.nb_total_clones, &app.nb_additional_elevators, &app.nb_elevators);
-    app.elev = (t_elev*)malloc(app.nb_elevators * sizeof(t_elev));
-    i = 0;
-    while (i < app.nb_elevators)
-    {
-        scanf("%d%d", &app.elev[i].y, &app.elev[i].x);
-        i++;
-    }
-    ft_set_current_elev(&app, 0);
-    ft_debug(&app);
-    ft_loop(&app);
+    return 0;
 }
